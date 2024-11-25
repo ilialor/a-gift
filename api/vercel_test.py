eca4554ed080c7af082a2ab4056c5a3676b471b9
Ilia Agafonov <ilia_agafonov@inbox.ru>
2024-11-25 19:27:01 +0300

    test11

--- api/vercel_test.py
+++ api/vercel_test.py
@@ -42,7 +42,7 @@ async def health():
     }
 
 # Database config
-DATABASE_URL = f"postgresql+asyncpg://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}?sslmode=require"
+DATABASE_URL = f"postgresql+asyncpg://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
